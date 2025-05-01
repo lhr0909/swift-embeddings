a75bfd026efac785f1376b31f48e6dfd97682505
Simon Liang <simon@x-tech.io>
2025-05-02 01:34:12 +0800

    make XMLRobertaTokenizer public

--- Sources/Embeddings/Tokenizer/XLMRobetaTokenizer.swift
+++ Sources/Embeddings/Tokenizer/XLMRobetaTokenizer.swift
@@ -3,7 +3,7 @@ import SentencepieceTokenizer
 import Synchronization
 
 @available(macOS 15.0, iOS 18.0, tvOS 18.0, visionOS 2.0, watchOS 11.0, *)
-final class XLMRobetaTokenizer: Sendable {
+public final class XLMRobetaTokenizer: Sendable {
     /// TODO: Make `SentencepieceTokenizer` conform to `Sendable`
     private let tokenizer: Mutex<SentencepieceTokenizer>
     private let addedTokens: [String: Int]
